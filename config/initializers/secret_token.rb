108eef322af03811de6c2bc2959937204c333f0f
sparkwire <sparkwire@0f2d62d2-9d44-4457-99fc-0b83e78a89e1>
2010-12-07 16:29:12 +0000

    Created new application
    git-svn-id: https://subversion.assembla.com/svn/sparkwire@225 0f2d62d2-9d44-4457-99fc-0b83e78a89e1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trunk::Application.config.secret_token = 'e1b4e06ebb6d1f3c4443e56abc5bbd238b804ad697847e9b5ea4a3ba58267817b512ea6362fa2a203410455ac801044460445578e2a7793d430dd77ca7785580'
