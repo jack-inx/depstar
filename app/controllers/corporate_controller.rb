db8270dc86c89130ed45272d66cbec99286c68e6
Charles Palleschi <charles.palleschi@sparkwiresolutions.com>
2011-02-10 10:22:52 -0500

    Added a "will" between "representative" and "contact"

--- app/controllers/corporate_controller.rb
+++ app/controllers/corporate_controller.rb
@@ -7,7 +7,7 @@ class CorporateController < ApplicationController
       @corporate_message = CorporateMessage.new(params[:corporate_message])  
     
       if @corporate_message.valid?
-        flash[:notice] =  "We have recieved your request. A Depstar representative contact you shortly."
+        flash[:notice] =  "We have recieved your request. A Depstar representative will contact you shortly."
       
         UserMailer.corporate_contact_email(@corporate_message).deliver
         
