2308d9bf8db0051c7f9199a893422358c68ad4f1
Charles Palleschi <charles.palleschi@sparkwiresolutions.com>
2012-02-27 17:18:11 -0500

    paginate shipping requests index

--- app/controllers/shipping_details_controller.rb
+++ app/controllers/shipping_details_controller.rb
@@ -4,8 +4,9 @@ class ShippingDetailsController < ApplicationController
   
   # GET /shipping_details
   # GET /shipping_details.xml
-  def index
-    @shipping_details = ShippingDetail.find(:all, :order => "created_at desc")
+  def index  
+    @shipping_details = ShippingDetail.paginate(:page => params[:page])
+    # @shipping_details = ShippingDetail.find(:all, :order => "created_at desc")
 
     respond_to do |format|
       format.html # index.html.erb
