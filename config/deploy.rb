45e7dfe62a4a5aa46c711ee9884d1aab6c134b87
Charles Palleschi <charles.palleschi@sparkwiresolutions.com>
2012-05-15 11:17:22 -0400

    Updated deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,8 +29,8 @@ set :bundle_roles,    {:except => {:no_release => true}} # e.g. [:app, :batch]
 
 require "rvm/capistrano"
 
-#set :rvm_ruby_string, '1.9.3'
-#set :rvm_type, :user  # Don't use system-wide RVM
+set :rvm_ruby_string, '1.9.3'
+set :rvm_type, :system
 
 namespace :rvm do
   desc 'Trust rvmrc file'
