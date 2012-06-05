1a6665404991e48f4a16d80f119684239143075a
Charles Palleschi <charles.palleschi@sparkwiresolutions.com>
2012-06-05 12:29:11 -0400

    Typo

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -49,7 +49,7 @@ Trunk::Application.configure do
 
   Stamps.configure do |config|
 	  config.integration_id = '904e44fc-b53f-44ae-bf2a-2d265241e389'
-	  onfig.username       = 'jacobresnek'
+	  config.username       = 'jacobresnek'
 	  config.password       = 'jake1878'
 	  #config.username       = 'depstar'
 	  #config.password       = 'postage1'
