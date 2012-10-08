83e839e77464c02c29e83ad0d7687220aa378c10
Charles Palleschi <charles.palleschi@sparkwiresolutions.com>
2012-10-08 17:46:20 -0400

    Added v2 contact page

--- app/models/contact_message.rb
+++ app/models/contact_message.rb
@@ -3,7 +3,7 @@ class ContactMessage
   include ActiveModel::Conversion
   extend ActiveModel::Naming
   
-  attr_accessor :name, :email, :phone, :subject, :message
+  attr_accessor :name, :email, :phone, :subject, :contact, :message
   
   validates_presence_of :name, :email, :phone
   validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
