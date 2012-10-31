3e508ca9ef24cf1944e1481e47b071b1cb7998db
Charles Palleschi <charles.palleschi@sparkwiresolutions.com>
2012-10-30 22:05:10 -0400

    Bug fixes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,9 @@ class HomeController < ApplicationController
   autocomplete :product, :name, :full => true
   
   def index     
-        @categories = Category.find_all_by_is_popular(true)
+    @categories = Category.find_all_by_is_popular(true)
         
-        @products = Product.find_all_by_is_popular(true)
+    #@products = Product.find_all_by_is_popular(true)
   end
   
 end
--- app/controllers/how_it_works_controller.rb
+++ app/controllers/how_it_works_controller.rb
@@ -1,7 +1,8 @@
 class HowItWorksController < ApplicationController
 
   def index
-    @products = Product.find_all_by_is_popular(true)
+    #@products = Product.find_all_by_is_popular(true)
+    @categories = Category.find_all_by_is_popular(true)
     
     respond_to do |format|
       format.html 
