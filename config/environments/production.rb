ef78b45565c76aeb013f7a92b4f7e1c5d2eb6afc
milap-inheritx <milap@inheritx.com>
2012-11-16 19:50:46 +0530

    change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Trunk::Application.configure do
    config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
     
   # For nginx:
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
