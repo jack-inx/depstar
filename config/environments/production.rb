2fca3986abc3dae6f3aba8a77c9ad1ea1930e433
ror_inx <ror_inx@ROR09.(none)>
2012-11-16 16:12:10 +0530

    Working on Searching Functionality

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,7 @@ Trunk::Application.configure do
   config.action_dispatch.x_sendfile_header = "X-Sendfile"
 
   # Compress JavaScripts and CSS
-  # config.assets.compress = true
+  config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
