c5893ab61cfb3171284fce30cfa9d332d43815d3
milap-inheritx <milap@inheritx.com>
2012-11-20 17:54:01 +0530

    change

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -22,7 +22,6 @@ class CategoriesController < ApplicationController
 
     @manufacturer_list = @category.manufacturers
 
-    
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @category }
