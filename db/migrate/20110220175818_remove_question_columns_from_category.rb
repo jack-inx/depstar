edfba33e6f90f2570959e764d637a98977d02e87
ror_inx <ror_inx@ROR09.(none)>
2012-11-21 11:39:11 +0530

    Migration Problem fixing

--- db/migrate/20110220175818_remove_question_columns_from_category.rb
+++ db/migrate/20110220175818_remove_question_columns_from_category.rb
@@ -11,14 +11,14 @@ class RemoveQuestionColumnsFromCategory < ActiveRecord::Migration
     remove_column :categories, :question_2_option_2_multiplier
 
     #remove_column :categories, :question_3_is_enabled, :boolean
-    remove_column :categories, :question_3_name, :string
-    remove_column :categories, :question_3_option_1_multiplier
-    remove_column :categories, :question_3_option_2_multiplier
-    remove_column :categories, :question_3_option_3_multiplier
-    remove_column :categories, :question_3_option_4_multiplier
+    #remove_column :categories, :question_3_name, :string
+    #remove_column :categories, :question_3_option_1_multiplier
+    #remove_column :categories, :question_3_option_2_multiplier
+    #remove_column :categories, :question_3_option_3_multiplier
+    #remove_column :categories, :question_3_option_4_multiplier
 
-    remove_column :categories, :question_4_is_enabled
-    remove_column :categories, :question_4_name
+    #remove_column :categories, :question_4_is_enabled
+    #remove_column :categories, :question_4_name
   end
 
   def self.down
@@ -32,14 +32,14 @@ class RemoveQuestionColumnsFromCategory < ActiveRecord::Migration
     add_column :categories, :question_2_option_1_multiplier, :float
     add_column :categories, :question_2_option_2_multiplier, :float
 
-    add_column :categories, :question_3_is_enabled, :boolean
-    add_column :categories, :question_3_name, :string
-    add_column :categories, :question_3_option_1_multiplier, :float
-    add_column :categories, :question_3_option_2_multiplier, :float
-    add_column :categories, :question_3_option_3_multiplier, :float
-    add_column :categories, :question_3_option_4_multiplier, :float
+    #add_column :categories, :question_3_is_enabled, :boolean
+    #add_column :categories, :question_3_name, :string
+    #add_column :categories, :question_3_option_1_multiplier, :float
+    #add_column :categories, :question_3_option_2_multiplier, :float
+    #add_column :categories, :question_3_option_3_multiplier, :float
+    #add_column :categories, :question_3_option_4_multiplier, :float
 
-    add_column :categories, :question_4_is_enabled, :boolean
-    add_column :categories, :question_4_name, :string
+    #add_column :categories, :question_4_is_enabled, :boolean
+    #add_column :categories, :question_4_name, :string
   end
 end
