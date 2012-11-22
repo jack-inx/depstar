1359a1b03f559e99325ed609894f67bda93a94f3
milap-inheritx <milap@inheritx.com>
2012-11-22 11:14:01 +0530

    update

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-  before_filter :authorize, :except => [:index, :show, :grades,:search_filter]
+  before_filter :authorize, :except => [:index, :show, :grades,:search_filter,:carrier_product]
   before_filter :xml_authorize, :include => [:index, :grades]
   #add_breadcrumb "Categories", :root_path
 
