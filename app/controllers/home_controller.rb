1c72e6bf6283756b884d74a0ced059b0bab17e9e
ror_inx <ror_inx@ROR09.(none)>
2012-11-26 20:25:23 +0530

    Search Functionality and Admin Functionality Done

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   autocomplete :product, :name, :full => true
   
   def index     
-    @categories = Category.find_all_by_is_popular(true)
+    @categories = Category.find_all
     @blogs = Blog.order("created_at").limit(4)    
     #@products = Product.find_all_by_is_popular(true)
   end
