4b4a950f63ac635b303a719e5ea9f1c279af6ba6
ror_inx <ror_inx@ROR09.(none)>
2012-12-06 15:21:49 +0530

    Home page Edited with Popular categories removed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,33 +1,32 @@
 require 'open-uri'
 require 'hpricot'
 
-class HomeController < ApplicationController  
+class HomeController < ApplicationController
+  
   autocomplete :product, :name, :full => true
   
-  def index     
+  def index
     @categories = Category.all
-    @shippings = ShippingDetail.order("created_at ASC").limit(3)
-    @blogs = Blog.order("created_at").limit(4)    
+    @shippings = ShippingDetail.order("created_at DESC").limit(3)
+    @blogs = Blog.order("created_at").limit(4)
 
-    doc = open("http://depstar.com/blog/") { |f|
-    Hpricot(f) 
-}
- (doc/".description-box").inner_html
- @title=[]
- @posted_by=[]
- @image_url=[]
- (doc/".description-box/h2/a").each do |f|
- 	@title << f
- end
- (doc/".description-box/.date-box").each do |posted|
- 	@posted_by << posted.html
- end
-
-  (doc/".description-box/.info-box/img@src").each do |f|
-
-  	@image_url << f.attributes['src']
+    doc = open("http://depstar.com/blog/") { |f| Hpricot(f)  }
+    
+    @title=[]
+    @posted_by=[]
+    @image_url=[]
+    
+    doc = open("http://depstar.com/blog/") { |f| Hpricot(f)  }
+    
+    (doc/".description-box").each do |box|      
+        @title << (box/"h2").html
+        @posted_by << (box/".date-box").html
+        if !(box/".info-box/img").blank?
+          @image_url << (box/".info-box/img").first.attributes['src']
+        else
+          @image_url << ""
+        end
+    end
   end
 
- end
-  
 end
