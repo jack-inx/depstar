75e3cd8eab699ac4ccb0aff5ee77705e715f6d2c
ror_inx <ror_inx@ROR09.(none)>
2012-12-26 20:29:25 +0530

    changes in shipping details page for device

--- app/admin/shipping_details.rb
+++ app/admin/shipping_details.rb
@@ -1,6 +1,8 @@
 ActiveAdmin.register ShippingDetail do
   menu :label => "Shipping Requests"
-  index do                            
+  index do       
+    menu :label => "Shipping Requests"
+                
     column :first_name
     column :product
     column :email
@@ -10,8 +12,7 @@ ActiveAdmin.register ShippingDetail do
     column :final_offer
     column :phone
     column :payment_method  
-   
-    
+
     default_actions                   
   end                                 
   filter :first_name
@@ -21,28 +22,34 @@ ActiveAdmin.register ShippingDetail do
   filter :email  
     
    form do |f|
+    f.inputs :payment_method
+    f.has_many :devices do |dev|
 
-    f.inputs :first_name
-    f.inputs :last_name
+      dev.input :name
+      dev.input :status_code, :as => :select, :collection =>ShippingDetail::ShippingStatus 
+      dev.input :serial, :input_html => { :size => 10 }
+      dev.input :offer
+      dev.input :final_offer
+      dev.input :notes
+    end
     f.inputs :email
+    f.inputs :first_name
+    f.inputs :last_name    
     f.inputs :phone  
     f.inputs :address1
     f.inputs :address2   
     f.inputs :city
     f.inputs :state
     f.inputs :zip      
-    f.inputs :uuid
-    f.inputs :serial
-    f.inputs :referer
-    f.inputs :offer
-    f.inputs :final_offer        
-    f.inputs :payment_method
     f.inputs :paypal_email
-    
+    f.inputs :uuid
     f.inputs "Shipping" do
       f.input :status_code, :as => :select, :collection =>ShippingDetail::ShippingStatus   
     end
-    
+    f.inputs :serial
+    f.inputs :offer
+    f.inputs :final_offer        
+    f.inputs :referer        
     f.inputs :notes, :as => :textarea
       
     f.actions                         
@@ -50,7 +57,7 @@ ActiveAdmin.register ShippingDetail do
   
   show do
     div :class => 'panel' do
-      h3 'Shipping Details'
+      h3 'Shipping Requests'
       div :class => 'panel_contents' do
         div :class => 'attributes_table shipping_detail' do
           table do
@@ -134,7 +141,43 @@ ActiveAdmin.register ShippingDetail do
               th { 'Notes' }
               td { shipping_detail.notes }
             end            
-            
+            if !shipping_detail.devices.map(&:name).first.nil?
+              tr do
+                th { 'Device name' }
+                td { shipping_detail.devices.map(&:name).join(' ')}              
+              end   
+            end
+            if !shipping_detail.devices.map(&:status_code).first.nil?            
+              tr do
+                th { 'Status code' }
+                td { shipping_detail.devices.map(&:status_code).join(' ')}              
+              end            
+            end
+           if !shipping_detail.devices.map(&:serial).first.nil?
+              tr do
+                th { 'Series' }
+                td { shipping_detail.devices.map(&:serial).join(' ') }              
+              end            
+            end
+           if !shipping_detail.devices.map(&:offer).first.nil?
+              tr do
+                th { 'Offer' }
+                td { shipping_detail.devices.map(&:offer).join(' ') }              
+              end            
+            end
+           if !shipping_detail.devices.map(&:final_offer).first.nil?
+              tr do
+                th { 'Final Offer' }
+                td { shipping_detail.devices.map(&:final_offer).join(' ') }              
+              end            
+            end
+           if !shipping_detail.devices.map(&:notes).first.nil?
+              tr do
+                th { 'Notes' }
+                td { shipping_detail.devices.map(&:Notes).join(' ')   }              
+              end            
+            end
+           
           end # table
         end # attributes_table
       end # panel_contents
